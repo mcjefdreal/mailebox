474be213e02b8e9a7724538a44dcde0278673f1e
Benjeouelle Lazaro <balazaro1@up.edu.ph>
2026-04-22 00:19:19 +0800

    chore: run formatters

--- webapp/src/routes/+page.svelte
+++ webapp/src/routes/+page.svelte
@@ -1,16 +1,28 @@
 <script>
-    import logo from '$lib/assets/mlb_logo.png';
-    import { resolve } from '$app/paths';
+	import logo from '$lib/assets/mlb_logo.png';
+	import { resolve } from '$app/paths';
 </script>
 
-<div class="from-mlb-blue/20 to-mlb-orange/20 flex h-screen flex-col items-center justify-center bg-linear-to-t">
-    <div class="bg-mlb-white m-6 w-80 rounded-xl p-5 flex flex-col items-center justify-center drop-shadow-md">
-        <img src={logo} alt="MaiLeBox logo" class="max-w-60 pt-5">
-        <p class="text-lg pt-8 font-extrabold"> Input Tracking Number </p>
-        <form class="flex flex-col p-3 items-center justify-center">
-            <input type="text" id="tracking_num" class="bg-mlb-gray/50 rounded-3xl py-1.5 px-2 text-center text-mlb-black">
-            <input type="submit" value="Submit" class="m-3 px-4 py-1.5 bg-mlb-orange rounded-2xl text-sm font-medium text-mlb-white hover:brightness-90 drop-shadow-sm">
-        </form>
-        <p class="text-sm text-mlb-black font-medium underline pt-10"> Log in as staff </p>
+<div
+	class="from-mlb-blue/20 to-mlb-orange/20 flex h-screen flex-col items-center justify-center bg-linear-to-t"
+>
+	<div
+		class="bg-mlb-white m-6 flex w-80 flex-col items-center justify-center rounded-xl p-5 drop-shadow-md"
+	>
+		<img src={logo} alt="MaiLeBox logo" class="max-w-60 pt-5" />
+		<p class="pt-8 text-lg font-extrabold">Input Tracking Number</p>
+		<form class="flex flex-col items-center justify-center p-3">
+			<input
+				type="text"
+				id="tracking_num"
+				class="bg-mlb-gray/50 text-mlb-black rounded-3xl px-2 py-1.5 text-center"
+			/>
+			<input
+				type="submit"
+				value="Submit"
+				class="bg-mlb-orange text-mlb-white m-3 rounded-2xl px-4 py-1.5 text-sm font-medium drop-shadow-sm hover:brightness-90"
+			/>
+		</form>
+		<p class="text-mlb-black pt-10 text-sm font-medium underline">Log in as staff</p>
 	</div>
 </div>
--- webapp/src/routes/tracking/+page.svelte
+++ webapp/src/routes/tracking/+page.svelte
@@ -1,38 +1,50 @@
 <script>
-    import logo from '$lib/assets/mlb_logo.png';
+	import logo from '$lib/assets/mlb_logo.png';
+	import StatusBar from '$lib/components/status_bar.svelte';
 
-    let {
-        tracking_num = 'RR123456785PH',
-        status = 'Delivered',
-        locker_num = '24',
-        deadline = '1'
-    } = $props();
+	let {
+		tracking_num = 'RR123456785PH',
+		status = 'Delivered',
+		locker_num = '24',
+		deadline = '1'
+	} = $props();
 </script>
 
-<div class="from-mlb-blue/20 to-mlb-orange/20 flex h-screen flex-col items-center justify-center bg-linear-to-t">
-    <div class="bg-mlb-white m-6 w-80 rounded-xl p-5 flex flex-col items-center justify-center drop-shadow-md">
-        <img src={logo} alt="MaiLeBox logo" class="max-w-50 pt-5">
-        
-        <div class="flex flex-col w-full p-3">
-            <p class="text-sm font-extrabold text-left py-1"> Tracking Number </p>
-            <p class="bg-mlb-gray/50 rounded-3xl py-1.5 px-2 text-center text-mlb-black font-bold"> {tracking_num} </p>
-        </div>
+<div
+	class="from-mlb-blue/20 to-mlb-orange/20 flex h-screen flex-col items-center justify-center bg-linear-to-t"
+>
+	<div
+		class="bg-mlb-white m-6 flex w-80 flex-col items-center justify-center rounded-xl p-5 drop-shadow-md"
+	>
+		<img src={logo} alt="MaiLeBox logo" class="max-w-50 pt-5" />
 
-        <div class="w-full p-3 grid grid-cols-3 gap-2 gap-x-2">
-            <div class="text-sm font-bold"> Status: </div>
-            <div class="col-span-2 text-sm"> {status} </div>
+		<div class="flex w-full flex-col p-3">
+			<p class="py-1 text-left text-sm font-extrabold">Tracking Number</p>
+			<p class="bg-mlb-gray/50 text-mlb-black rounded-3xl px-2 py-1.5 text-center font-bold">
+				{tracking_num}
+			</p>
+		</div>
 
-            <div class="text-sm font-bold"> Parcel at: </div>
-            <div class="col-span-2 text-sm underline"> #{locker_num} </div>
+		<div class="grid w-full grid-cols-3 gap-2 gap-x-2 p-3">
+			<div class="text-sm font-bold">Status:</div>
+			<div class="col-span-2 text-sm">{status}</div>
 
-            <div class="text-sm font-bold"> Claim in: </div>
-            <div class="col-span-2 text-sm"> {deadline} days </div>
-        </div>
+			<div class="text-sm font-bold">Parcel at:</div>
+			<div class="col-span-2 text-sm underline">#{locker_num}</div>
 
-        <div>
-            <p class="italic"> status tba </p>
-        </div>
+			<div class="text-sm font-bold">Claim in:</div>
+			<div class="col-span-2 text-sm">{deadline} days</div>
+		</div>
 
-        <button class="m-3 px-4 py-1.5 bg-mlb-orange rounded-2xl text-sm font-medium text-mlb-white hover:brightness-90 drop-shadow-sm"> Unlock </button>
+		<div class="py-4">
+			<!-- <p class="italic"> status tba </p> -->
+			<StatusBar />
+		</div>
+
+		<button
+			class="bg-mlb-orange text-mlb-white m-3 rounded-2xl px-4 py-1.5 text-sm font-medium drop-shadow-sm hover:brightness-90"
+		>
+			Unlock
+		</button>
 	</div>
 </div>
