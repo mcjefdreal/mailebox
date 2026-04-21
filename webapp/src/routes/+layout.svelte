1f130b1a8a2b0caba6d54147faa36bf87e0a35f4
kalatkarin <carlynecconsebido@gmail.com>
2026-04-21 21:31:13 +0800

    feat: set up Tailwind config

--- webapp/src/routes/+layout.svelte
+++ webapp/src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	import './layout.css';
 	import { PUBLIC_CONVEX_URL } from '$env/static/public';
 	import { setupConvex } from 'convex-svelte';
+	import '../app.css';
 
 	let { children } = $props();
 
