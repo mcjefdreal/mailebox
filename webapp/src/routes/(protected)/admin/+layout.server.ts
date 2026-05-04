0bf0a51fb4799b3c0532f7dbdf7315e5fc869d1d
Marcus Pascual <marcusjoaquim05@gmail.com>
2026-05-04 21:12:12 +0800

    feat: protect admin route in server load function

---
+++ webapp/src/routes/(protected)/admin/+layout.server.ts
@@ -0,0 +1,19 @@
+import type { LayoutServerLoad } from '../$types.js';
+import { redirect } from '@sveltejs/kit';
+import { ConvexHttpClient } from 'convex/browser';
+import { PUBLIC_CONVEX_URL } from '$env/static/public';
+import { api } from '$convex/_generated/api.js';
+
+export const load: LayoutServerLoad = async ({ locals }) => {
+    // check if the user is an admim here using a Convex query
+    const client = new ConvexHttpClient(PUBLIC_CONVEX_URL, { auth: locals.token });
+    
+    if (!locals.token) {
+        throw redirect(303, '/login');
+    }
+
+    const roleData = await client.query(api.auth.getUserRole, {});
+    if (!roleData || roleData.role !== "admin") {
+        throw redirect(303, '/login');
+    }
+};
