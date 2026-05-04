d472d8a79aacdaa6a69f783e8e4b47fdcc52b506
Marcus Pascual <marcusjoaquim05@gmail.com>
2026-05-04 21:25:04 +0800

    fix: check locals auth token before creating Convex auth client

--- webapp/src/routes/(protected)/admin/+layout.server.ts
+++ webapp/src/routes/(protected)/admin/+layout.server.ts
@@ -5,13 +5,12 @@ import { PUBLIC_CONVEX_URL } from '$env/static/public';
 import { api } from '$convex/_generated/api.js';
 
 export const load: LayoutServerLoad = async ({ locals }) => {
-    // check if the user is an admim here using a Convex query
-    const client = new ConvexHttpClient(PUBLIC_CONVEX_URL, { auth: locals.token });
-    
     if (!locals.token) {
         throw redirect(303, '/login');
     }
-
+    
+    // check if the user is an admim here using a Convex query
+    const client = new ConvexHttpClient(PUBLIC_CONVEX_URL, { auth: locals.token });
     const roleData = await client.query(api.auth.getUserRole, {});
     if (!roleData || roleData.role !== "admin") {
         throw redirect(303, '/login');
